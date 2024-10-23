8aee17c823e55875edcc5dc4793ae3d330fc7571
Beverly140562 <beverlysalde.140562@gmail.com>
2024-10-23 08:27:09 +0000

    commit

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 
-    let cartCount = 0;
+let cart = [];
 
-    function addToCart(productName) {
-      cartCount++;
-      document.getElementById('cart-count').innerText = cartCount;
-      alert(productName + ' has been added to your cart!');
-    }
+
+function addToCart(item) {
+    cart.push(item);
+    alert(`${item.name} has been added to your cart!`);
+    console.log(cart);
+}
+
+
+function init() {
+ 
+    const addToCartButtons = document.querySelectorAll('.btn');
+
+    addToCartButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const products = [
+                { name: 'Samsung', price: 30, stock: 10 },
+                { name: 'Iphone', price: 50, stock:12 },
+                { name: 'Infinix', price: 10, stock:20 },
+            ];
+            addToCart(products[index]);
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', init);
